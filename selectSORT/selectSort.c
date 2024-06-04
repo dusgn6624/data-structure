da8938e4406f42e19c2c1281d629f26b9fdd57a7
dusgn6624 <147192422+dusgn6624@users.noreply.github.com>
2024-06-04 11:04:18 +0900

    Add files via upload

---
+++ selectSORT/selectSort.c
@@ -0,0 +1,15 @@
+void SelectionSort(int a[], int size) {
+    int i, j, t, min, temp;
+
+    for (i = 0; i < size - 1; i++) {
+        min = i;
+        for (j = i + 1; j < size; j++) {
+            if (a[j] < a[min]) min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+        printf("\n");
+        for (t = 0; t < size; t++) printf("%3d ", a[t]);
+    }
+}
